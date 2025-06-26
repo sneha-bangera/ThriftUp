4c0f246e7401c57f41d42c5d082e0dbeb54a430f
Sneha Bangera <snehabangera05@gmail.com>
2025-06-26 23:46:02 +0530

    schema added

---
+++ src/models/Product.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product || mongoose.model("Product", productSchema);
